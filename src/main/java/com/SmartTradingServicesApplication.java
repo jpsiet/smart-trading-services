3de805e1c3566a2d85d905e153d063ca85218290
jpsiet <jpsiet@users.noreply.github.com>
2025-02-25 22:11:59 -0500

    inital code base

---
+++ src/main/java/com/SmartTradingServicesApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTradingServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTradingServicesApplication.class, args);
+	}
+
+}
